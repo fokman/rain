74370a00fe5fc34c05882fb29335b7c0f98481ed
ken <kun.liu@cm-dt.com>
2017-12-29 13:27:29 +0800

    init rain project

---
+++ rain-common/src/main/java/com/rain/common/RainCommonApplication.java
@@ -0,0 +1,12 @@
+package com.rain.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainCommonApplication.class, args);
+	}
+}
---
+++ rain-web/src/test/java/com/rain/web/RainWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rain.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RainWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
