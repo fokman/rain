66befc149b34bbf9f71e3ebcf7b7ad75ee18c80a
ken <kun.liu@cm-dt.com>
2018-02-26 16:04:39 +0800

    refactor ice client

--- rain-service/redis-service/src/main/java/com/rain/role/service/RedisCacheConfig.java
+++ rain-service/redis-service/src/main/java/com/rain/role/service/RedisCacheConfig.java
@@ -1,5 +1,12 @@
 package com.rain.role.service;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 /**
  * Project Name: Rain
  * <p>
@@ -7,5 +14,22 @@ package com.rain.role.service;
  * <p>
  * Copyright(c) 2018 Virtue Intelligent Network Ltd, co. All Rights Reserved.
  */
-public class RedisCacheConfig {
+
+public class RedisCacheConfig{
+
+    public CacheManager cacheManager(RedisTemplate redisTemplate){
+        CacheManager cacheManager = new RedisCacheManager(redisTemplate);
+        return cacheManager;
+    }
+
+    public RedisTemplate redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(factory);
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(redisSerializer);
+        redisTemplate.setHashKeySerializer(redisSerializer);
+        return  redisTemplate;
+    }
+
+
 }
