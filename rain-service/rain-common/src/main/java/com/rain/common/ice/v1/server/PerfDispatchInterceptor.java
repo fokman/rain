1d6194f16a3e372a5caa03df4b490b2251e6cba8
ken <liukun_iov@chinamobile.com>
2019-10-10 09:33:55 +0800

    make sure can works under deploy situation

--- rain-service/rain-common/src/main/java/com/rain/common/ice/v1/server/PerfDispatchInterceptor.java
+++ rain-service/rain-common/src/main/java/com/rain/common/ice/v1/server/PerfDispatchInterceptor.java
@@ -4,63 +4,59 @@ import Ice.DispatchInterceptor;
 import Ice.DispatchStatus;
 import Ice.Identity;
 import Ice.Request;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 
+@Slf4j
 public class PerfDispatchInterceptor extends DispatchInterceptor {
-	private static org.slf4j.Logger logger = LoggerFactory
-			.getLogger(PerfDispatchInterceptor.class);
 
-	private static final Map<Identity, Ice.Object> id2ObjectMAP = new java.util.concurrent.ConcurrentHashMap<Identity, Ice.Object>();
-	private static final PerfDispatchInterceptor self = new PerfDispatchInterceptor();
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+    private static final Map<Identity, Ice.Object> id2ObjectMAP = new java.util.concurrent.ConcurrentHashMap<>();
+    private static final PerfDispatchInterceptor self = new PerfDispatchInterceptor();
 
-	public static PerfDispatchInterceptor getINSTANCE() {
-		return self;
-	}
+    public static PerfDispatchInterceptor getINSTANCE() {
+        return self;
+    }
 
-	public static DispatchInterceptor addICEObject(Identity id,
-			Ice.Object iceObj) {
-		id2ObjectMAP.put(id, iceObj);
-		return self;
-	}
+    public static DispatchInterceptor addICEObject(Identity id,
+                                                   Ice.Object iceObj) {
+        id2ObjectMAP.put(id, iceObj);
+        return self;
+    }
 
-	/**
-	 * 此方法可以做任何拦截，类似AOP.
-	 */
-	@Override
-	public DispatchStatus dispatch(Request request) {
-		Identity theId = request.getCurrent().id;
-		// request.getCurrent().con会打印出 local address = 16.156.210.172:50907
-		// （回车换行） remote address = 16.156.210.172:51147 这样的信
-		// 其中 local address 为被访问的服务的地址端口，Remote Address为客户端的端口
-		String inf = "dispach req,method:" + request.getCurrent().operation
-				+ " service:" + theId.name + " server address:"
-				+ request.getCurrent().con;
-		
-		logger.info(inf + " begin");
+    /**
+     * 此方法可以做任何拦截，类似AOP.
+     */
+    @Override
+    public DispatchStatus dispatch(Request request) {
+        Identity theId = request.getCurrent().id;
+        // request.getCurrent().con会打印出 local address = 16.156.210.172:50907
+        // remote address = 16.156.210.172:51147 这样的信
+        // 其中 local address 为被访问的服务的地址端口，Remote Address为客户端的端口
+        String inf = "dispach req,method:" + request.getCurrent().operation
+                + " service:" + theId.name + " server address:"
+                + request.getCurrent().con;
 
-		try {
+        log.info("{} begin", inf);
 
-			DispatchStatus reslt = id2ObjectMAP.get(request.getCurrent().id)
-					.ice_dispatch(request);
-			logger.info(inf + " success");
-			return reslt;
-		} catch (RuntimeException e) {
-			logger.info(inf + " error " + e);
-			throw e;
+        try {
 
-		}
-	}
+            DispatchStatus reslt = id2ObjectMAP.get(request.getCurrent().id)
+                    .ice_dispatch(request);
+            log.info("{} success", inf);
+            return reslt;
+        } catch (RuntimeException e) {
+            log.info("{} error {}", inf, e);
+            throw e;
 
-	public static void removeICEObject(Identity id) {
-		logger.info("remove ice object " + id);
-		id2ObjectMAP.remove(id);
+        }
+    }
 
-	}
+    public static void removeICEObject(Identity id) {
+        log.info("remove ice object {}", id);
+        id2ObjectMAP.remove(id);
+
+    }
 
 }
